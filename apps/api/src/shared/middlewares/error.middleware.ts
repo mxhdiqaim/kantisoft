3b322a6be51999a4b7615ccc9bc3e2d5a3bdd9ac
mahdi <47560898+mxhdiqaim@users.noreply.github.com>
2026-07-28 22:06:36 +0100

    refactor: update global error handler to remove unused `NextFunction` parameter and simplify logging

--- apps/api/src/shared/middlewares/error.middleware.ts
+++ apps/api/src/shared/middlewares/error.middleware.ts
@@ -1,14 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { getEnvVariable } from "../utils";
 import { AppError } from "../errors/custom.error";
 
-export const globalErrorHandler = (
-    err: Error,
-    req: Request,
-    res: Response,
-    next: NextFunction,
-) => {
+export const globalErrorHandler = (err: Error, req: Request, res: Response) => {
     const NODE_ENV = getEnvVariable("NODE_ENV") || "development";
 
     let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
@@ -20,9 +15,7 @@ export const globalErrorHandler = (
     }
 
     if (NODE_ENV === "development") {
-        console.error(
-            `\n🚨 [ERROR] [${req.method}] ${req.path} - Status: ${statusCode}`,
-        );
+        console.error(`\n🚨 [ERROR] [${req.method}] ${req.path} - Status: ${statusCode}`);
         console.error(`Message: ${err.message}`);
         if (err.stack) {
             console.error(`Stack Trace:\n${err.stack}\n`);
